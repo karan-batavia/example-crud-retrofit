baf0fd970ad5ce08b48d44a50110b7b33103ad81
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-05 10:54:19 +0700

    chore (get): add get with query, query map and path

--- app/src/main/java/com/ichwan/crud/restful/api/ApiService.kt
+++ app/src/main/java/com/ichwan/crud/restful/api/ApiService.kt
@@ -1,16 +1,30 @@
 package com.ichwan.crud.restful.api
 
+import com.ichwan.crud.restful.response.CommentResponse
 import com.ichwan.crud.restful.response.PostResponse
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+import java.lang.reflect.Parameter
 
 interface ApiService {
 
     @GET("posts")
     fun getPosts(): Call<List<PostResponse>>
+
+    @GET("posts/{postId}/comments")
+    fun getPostsById(@Path("postId") postId: Int): Call<List<CommentResponse>>
+
+    @GET("posts")
+    fun getPostsByUserId(@Query("userId") userId: String): Call<List<PostResponse>>
+
+    @GET("posts")
+    fun getPostsByQueryMap(@QueryMap parameter: HashMap<String, String>): Call<List<PostResponse>>
 
     @FormUrlEncoded
     @POST("posts")
